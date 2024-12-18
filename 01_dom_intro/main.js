c1c96316e1517bf8c4b26d565b0a4a5f846bb63b
learnzdevelopmenthub <learnzdevelopmenthub@gmail.com>
2024-12-18 20:37:07 +0530

    added first project

---
+++ 01_dom_intro/main.js
@@ -0,0 +1,33 @@
+// function statement
+// function changeText() {
+//     document.querySelector('#paragraph1').innerHTML = "paragraph 1 changed"
+// }
+
+
+// let changeText = function () {
+//     document.querySelector('#paragraph1').innerHTML = "paragraph 1 changed"   
+// }
+
+// arrow function
+let changeText = () => {
+    document.querySelector('#paragraph1').innerHTML = "paragraph 1 changed" 
+}
+
+// onclick in javascript
+
+// document.querySelector('#changeBtn').onclick = function(){
+//     changeText()
+// }
+
+let changeBtn = document.querySelector('#changeBtn')
+
+// using event listner
+changeBtn.addEventListener('click', () => {
+    changeText()
+})
+
+changeBtn.addEventListener('mouseover', () => {
+    console.log("Mouse hovered")
+})
+
+// event, event handling, way of event handlings, variable, way to write functions
